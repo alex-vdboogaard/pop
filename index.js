361d4413cc9c0d96f2b837e14f3506bc1926adc8
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-10-01 08:59:30 +0200

    add warning pop

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ function delayedRemove() {
     }, 3000);
 }
 
+//positions: top-right, bottom-right, bottom
 export function simplePop(type, message, position = "top") {
     closeAllPop();
     if (type === "success") type = "pop-success";
     if (type === "error") type = "pop-error";
+    if (type === "warning") type = "pop-warning";
     const body = document.querySelector("body");
     const alert = document.createElement("div");
     alert.classList.add("pop-top", "pop", type);
@@ -30,7 +32,7 @@ export function simplePop(type, message, position = "top") {
         </svg>
         <p>${message}</p>`;
     }
-    else {
+    else if (type === "pop-error") {
         alert.innerHTML = `<svg style="margin-right:15px" class="pop-svg" width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_2287_12)">
         <path d="M45 0C20.1445 0 0 20.1445 0 45C0 69.8555 20.1445 90 45 90C69.8555 90 90 69.8555 90 45C90 20.1445 69.8555 0 45 0ZM11.25 45C11.25 26.3496 26.3672 11.25 45 11.25C52.4004 11.25 59.2383 13.6582 64.793 17.7012L17.7012 64.793C13.6582 59.2383 11.25 52.4004 11.25 45ZM45 78.75C37.5996 78.75 30.7617 76.3418 25.207 72.2988L72.2988 25.207C76.3418 30.7793 78.75 37.5996 78.75 45C78.75 63.6504 63.6328 78.75 45 78.75Z" fill="white"/>
@@ -41,6 +43,11 @@ export function simplePop(type, message, position = "top") {
         </clipPath>
         </defs>
         </svg><p>${message}</p>`;
+    }
+    else if (type === "pop-warning") {
+        alert.innerHTML = `<svg style="margin-right:15px" class="pop-svg" width="90" height="90" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 21L12 2L23 21H1ZM12 18C12.2833 18 12.521 17.904 12.713 17.712C12.905 17.52 13.0007 17.2827 13 17C12.9993 16.7173 12.9033 16.48 12.712 16.288C12.5207 16.096 12.2833 16 12 16C11.7167 16 11.4793 16.096 11.288 16.288C11.0967 16.48 11.0007 16.7173 11 17C10.9993 17.2827 11.0953 17.5203 11.288 17.713C11.4807 17.9057 11.718 18.0013 12 18ZM11 15H13V10H11V15Z" fill="white"/>
+</svg><p>${message}</p>`;
     }
 
     body.appendChild(alert);
