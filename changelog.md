634a02d48eadb2d3a4371210708980b1932a5642
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-10-06 22:21:37 +0200

    add options object parameter for background-colour, close icon, and time

---
+++ changelog.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [2.0.0] - YYYY-MM-
+### Added
+- options object to simplePop to allow you to further customise your messages.
+- options include whether a close icon shows, time it takes to disappear, background-color, and position.
+
+## [1.0.0] - 2024-09-28
+- Initial release.
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function closeAllPop() {
     document.querySelectorAll(".pop").forEach(pop => { pop.remove(); });
 }
 
-//this makes the popup move up out of the screen then disappear
-function delayedRemove() {
+function delayedRemove(timeMS = 3000) {
     const popElements = document.querySelectorAll(".pop");
     setTimeout(function () {
         popElements.forEach(pop => {
@@ -13,16 +12,22 @@ function delayedRemove() {
                 pop.remove();
             }, { once: true });
         });
-    }, 3000);
+    }, timeMS);
 }
 
-//positions: top-right, bottom-right, bottom
-export function simplePop(type, message) {
+export function simplePop(type, message, options = {}) {
     closeAllPop();
 
     const body = document.querySelector("body");
     const alert = document.createElement("div");
-    alert.classList.add("pop-top", "pop", `pop-${type}`);
+
+    const closeIcon = options.close ? `<svg class='pop-close' onClick="this.parentNode.remove()" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_2565_24)"><path d="M1.4 14L0 12.6L5.6 7L0 1.4L1.4 0L7 5.6L12.6 0L14 1.4L8.4 7L14 12.6L12.6 14L7 8.4L1.4 14Z" fill="white"/></g><defs><clipPath id="clip0_2565_24"><rect width="14" height="14" fill="white"/></clipPath></defs></svg>` : '';
+    const timeMS = options.time ? options.time : 3000;
+    const background = options.background ? options.background : "";
+    const position = options.position ? `pop-${options.position}` : "pop";
+
+    alert.classList.add("pop-top", "pop", `pop-${type}`, position);
+    alert.style.backgroundColor = background;
 
     switch (type) {
         case "success":
@@ -55,7 +60,7 @@ export function simplePop(type, message) {
         default:
             break;
     }
-
+    alert.innerHTML += closeIcon;
     body.appendChild(alert);
 
     setTimeout(function () {
@@ -63,7 +68,8 @@ export function simplePop(type, message) {
         alert.style.top = "10px";
     }, 100);
 
-    delayedRemove();
+
+    delayedRemove(timeMS);
 }
 
 export async function confirmPop(message) {
